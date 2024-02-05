ca4a70b191d9075de5c0bec95c9e5a998443d4d3
gamehole <741419087@qq.com>
2024-02-05 10:59:17 +0800

    调整idle

--- Assets/RPG/Scripts/State/IdleState.cs
+++ Assets/RPG/Scripts/State/IdleState.cs
@@ -5,11 +5,6 @@ namespace RPG
 {
     public class IdleState: State
     {
-        public IdleState()
-        {
-            transations.Add(new TransitionToSkill { _this = this });
-            transations.Add(new TransitionToMove());
-        }
         public override void Start()
         {
             character.animator.Idle();
