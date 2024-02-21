b0f46532914f98d756e5078bc56ad552d6e33b34
gamehole <741419087@qq.com>
2024-02-21 10:25:24 +0800

    分离测试

--- Assets/RPG/UnitTest/TestStates.cs
+++ Assets/RPG/UnitTest/TestStates.cs
@@ -9,7 +9,6 @@ using UnityEngine;
 
 namespace UnitTest
 {
-
     internal class TestStates
     {
         private TestingAnimator anim;
@@ -69,35 +68,6 @@ namespace UnitTest
             Assert.AreEqual("hit", anim.log);
         }
         [Test]
-        public void testFinishTransitionBlocker()
-        {
-            var mat = new StateMatchine();
-            var timer = new Timer { duration = 1 };
-            var tran = new FinishTransitionBlocker(timer);
-            tran.SetCharacter(cha);
-            tran.SetMatchine(mat);
-            for (int i = 0; i < 2; i++)
-            {
-                timer.Update(0.5f);
-                Assert.AreEqual(i == 0, tran.isVailed());
-            }
-            tran.Switch();
-            Assert.IsNull(mat.runingState);
-        }
-        [Test]
-        public void testFinishTransition()
-        {
-            var timer = new Timer { duration=1};
-            var tran = new FinishTransition(StateName.Idle, timer);
-            Assert.AreEqual(StateName.Idle, tran.stateName);
-            tran.SetCharacter(cha);
-            for (int i = 0; i < 2; i++)
-            {
-                timer.Update(0.5f);
-                Assert.AreEqual(i == 1, tran.isVailed());
-            }
-        }
-        [Test]
         public void testTimer()
         {
             var timer = new Timer { duration = 1 };
@@ -109,61 +79,6 @@ namespace UnitTest
             }
         }
         [Test]
-        public void testTransitionToHit()
-        {
-            var tran = new TransitionToHit();
-            Assert.AreEqual(StateName.Hit, tran.stateName);
-            tran.SetCharacter(cha);
-            var hit = new HitInfo { demage = 1 };
-            cha.Hit(hit);
-            for (int i = 0; i < 2; i++)
-            {
-                Assert.AreEqual(i == 0, tran.isVailed());
-            }
-            cha.hittable = new Unhittable();
-            cha.Hit(hit);
-            for (int i = 0; i < 2; i++)
-            {
-                Assert.IsFalse(tran.isVailed());
-            }
-        }
-        [Test]
-        public void testTransitionToIdle()
-        {
-            var tran = new TransitionToIdle();
-            Assert.AreEqual(StateName.Idle, tran.stateName);
-            tran.SetCharacter(cha);
-            for (int i = 0; i < 2; i++)
-            {
-                input.moveDir = new Vector2(i, 0);
-                Assert.AreEqual(i == 0, tran.isVailed());
-            }
-        }
-        [Test]
-        public void testTransitionToMove()
-        {
-            var tran = new TransitionToMove();
-            tran.SetCharacter(cha);
-            Assert.AreEqual(StateName.Move, tran.stateName);
-            for (int i = 0; i < 2; i++)
-            {
-                input.moveDir = new Vector2(i, 0);
-                Assert.AreEqual(i != 0, tran.isVailed());
-            }
-        }
-        [Test]
-        public void testTransitionToSkill()
-        {
-            var tran = new TransitionToSkill();
-            tran.SetCharacter(cha);
-            Assert.AreEqual(StateName.Skill, tran.stateName);
-            for (int i = 0; i < 2; i++)
-            {
-                input.isAttact = i == 0;
-                Assert.AreEqual(i == 0, tran.isVailed());
-            }
-        }
-        [Test]
         public void testDeadState()
         {
             var state = new DeadState();
@@ -171,18 +86,6 @@ namespace UnitTest
             state.Start();
             Assert.AreEqual("dead", anim.log);
             Assert.AreEqual(typeof(NoneHitter), cha.hitter.GetType());
-        }
-        [Test]
-        public void testTransitionToDead()
-        {
-            var tran = new TransitionToDead();
-            tran.SetCharacter(cha);
-            Assert.AreEqual(StateName.Dead, tran.stateName);
-            for (int i = -1; i < 2; i++)
-            {
-                cha.hp = i;
-                Assert.AreEqual(i <= 0, tran.isVailed());
-            }
         }
         [Test]
         public void testReviveState()
@@ -199,42 +102,6 @@ namespace UnitTest
             Assert.AreSame(cha.hitter,cha.defaultHitter);
         }
         [Test]
-        public void testTransitionToRevive()
-        {
-            var tran = new TransitionToRevive();
-            tran.SetCharacter(cha);
-            Assert.AreEqual(StateName.Revive, tran.stateName);
-            for (int i = -1; i < 2; i++)
-            {
-                cha.hp = i;
-                Assert.AreEqual(i > 0, tran.isVailed());
-            }
-        }
-        [Test]
-        public void testTransitionDefenseToIdle()
-        {
-            var tran = new TransitionDefenseToIdle();
-            tran.SetCharacter(cha);
-            Assert.AreEqual(StateName.Idle, tran.stateName);
-            for (int i = 0; i < 2; i++)
-            {
-                input.isDefense = i == 0;
-                Assert.AreEqual(i == 1, tran.isVailed());
-            }
-        }
-        [Test]
-        public void testTransitionToDefense()
-        {
-            var tran = new TransitionToDefense();
-            tran.SetCharacter(cha);
-            Assert.AreEqual(StateName.Defense, tran.stateName);
-            for (int i = 0; i < 2; i++)
-            {
-                input.isDefense = i == 0;
-                Assert.AreEqual(i == 0, tran.isVailed());
-            }
-        }
-        [Test]
         public void testDefenseState()
         {
             var state = new DefenseState();
@@ -247,18 +114,6 @@ namespace UnitTest
             Assert.AreSame(hitter, state.hitter);
             state.End();
             Assert.AreSame(hitter,cha.hitter);
-        }
-        [Test]
-        public void testTransitionToBreakDefense()
-        {
-            var tran = new TransitionToBreakDefense();
-            tran.SetCharacter(cha);
-            Assert.AreEqual(StateName.BreakDefense, tran.stateName);
-            for (int i = 0; i < 2; i++)
-            {
-                cha.strength = i;
-                Assert.AreEqual(i == 0, tran.isVailed());
-            }
         }
         [Test]
         public void testBreakDefenseState()
---
+++ Assets/RPG/UnitTest/TestTransitions.cs
@@ -0,0 +1,171 @@
+﻿using NUnit.Framework;
+using RPG;
+using UnityEngine;
+
+namespace UnitTest
+{
+    internal class TestTransitions
+    {
+        private TestingAnimator anim;
+        private TestingActionInput input;
+        private TestDeltaTime deltaTime;
+        private Character cha;
+
+        [SetUp]
+        public void set()
+        {
+            anim = new TestingAnimator();
+            input = new TestingActionInput();
+            deltaTime = new TestDeltaTime();
+            cha = new Character(input, anim, deltaTime);
+            anim.log = null;
+            deltaTime._value = 0.5f;
+        }
+        [Test]
+        public void testFinishTransitionBlocker()
+        {
+            var mat = new StateMatchine();
+            var timer = new Timer { duration = 1 };
+            var tran = new FinishTransitionBlocker(timer);
+            tran.SetCharacter(cha);
+            tran.SetMatchine(mat);
+            for (int i = 0; i < 2; i++)
+            {
+                timer.Update(0.5f);
+                Assert.AreEqual(i == 0, tran.isVailed());
+            }
+            tran.Switch();
+            Assert.IsNull(mat.runingState);
+        }
+        [Test]
+        public void testFinishTransition()
+        {
+            var timer = new Timer { duration = 1 };
+            var tran = new FinishTransition(StateName.Idle, timer);
+            Assert.AreEqual(StateName.Idle, tran.stateName);
+            tran.SetCharacter(cha);
+            for (int i = 0; i < 2; i++)
+            {
+                timer.Update(0.5f);
+                Assert.AreEqual(i == 1, tran.isVailed());
+            }
+        }
+        [Test]
+        public void testTransitionToHit()
+        {
+            var tran = new TransitionToHit();
+            Assert.AreEqual(StateName.Hit, tran.stateName);
+            tran.SetCharacter(cha);
+            var hit = new HitInfo { demage = 1 };
+            cha.Hit(hit);
+            for (int i = 0; i < 2; i++)
+            {
+                Assert.AreEqual(i == 0, tran.isVailed());
+            }
+            cha.hittable = new Unhittable();
+            cha.Hit(hit);
+            for (int i = 0; i < 2; i++)
+            {
+                Assert.IsFalse(tran.isVailed());
+            }
+        }
+        [Test]
+        public void testTransitionToIdle()
+        {
+            var tran = new TransitionToIdle();
+            Assert.AreEqual(StateName.Idle, tran.stateName);
+            tran.SetCharacter(cha);
+            for (int i = 0; i < 2; i++)
+            {
+                input.moveDir = new Vector2(i, 0);
+                Assert.AreEqual(i == 0, tran.isVailed());
+            }
+        }
+        [Test]
+        public void testTransitionToMove()
+        {
+            var tran = new TransitionToMove();
+            tran.SetCharacter(cha);
+            Assert.AreEqual(StateName.Move, tran.stateName);
+            for (int i = 0; i < 2; i++)
+            {
+                input.moveDir = new Vector2(i, 0);
+                Assert.AreEqual(i != 0, tran.isVailed());
+            }
+        }
+        [Test]
+        public void testTransitionToSkill()
+        {
+            var tran = new TransitionToSkill();
+            tran.SetCharacter(cha);
+            Assert.AreEqual(StateName.Skill, tran.stateName);
+            for (int i = 0; i < 2; i++)
+            {
+                input.isAttact = i == 0;
+                Assert.AreEqual(i == 0, tran.isVailed());
+            }
+        }
+
+        [Test]
+        public void testTransitionToDead()
+        {
+            var tran = new TransitionToDead();
+            tran.SetCharacter(cha);
+            Assert.AreEqual(StateName.Dead, tran.stateName);
+            for (int i = -1; i < 2; i++)
+            {
+                cha.hp = i;
+                Assert.AreEqual(i <= 0, tran.isVailed());
+            }
+        }
+        [Test]
+        public void testTransitionToRevive()
+        {
+            var tran = new TransitionToRevive();
+            tran.SetCharacter(cha);
+            Assert.AreEqual(StateName.Revive, tran.stateName);
+            for (int i = -1; i < 2; i++)
+            {
+                cha.hp = i;
+                Assert.AreEqual(i > 0, tran.isVailed());
+            }
+        }
+        [Test]
+        public void testTransitionDefenseToIdle()
+        {
+            var tran = new TransitionDefenseToIdle();
+            tran.SetCharacter(cha);
+            Assert.AreEqual(StateName.Idle, tran.stateName);
+            for (int i = 0; i < 2; i++)
+            {
+                input.isDefense = i == 0;
+                Assert.AreEqual(i == 1, tran.isVailed());
+            }
+        }
+        [Test]
+        public void testTransitionToDefense()
+        {
+            var tran = new TransitionToDefense();
+            tran.SetCharacter(cha);
+            Assert.AreEqual(StateName.Defense, tran.stateName);
+            for (int i = 0; i < 2; i++)
+            {
+                input.isDefense = i == 0;
+                Assert.AreEqual(i == 0, tran.isVailed());
+            }
+        }
+   
+        [Test]
+        public void testTransitionToBreakDefense()
+        {
+            var tran = new TransitionToBreakDefense();
+            tran.SetCharacter(cha);
+            Assert.AreEqual(StateName.BreakDefense, tran.stateName);
+            for (int i = 0; i < 2; i++)
+            {
+                cha.strength = i;
+                Assert.AreEqual(i == 0, tran.isVailed());
+            }
+        }
+    }
+}
